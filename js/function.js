e4b6b5f7ec0cae64e8d61ce8e77098c769f056c7
choisojin <choisojin@naver.com>
2025-05-15 17:39:24 +0900

    jquery to JS

--- js/function.js
+++ js/function.js
@@ -1,56 +1,68 @@
-$(function(){
-  $(window).on('load',function(){
-    new WOW().init();
+document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('load', function() {
+    if (typeof WOW === 'function') {
+      new WOW().init();
+    }
   });
-});//wow plugin 초기화
 
-$(function(){
-  //var
-  var $header = $('header');
-  var $mnu = $('header>.container>nav>.gnb>li>a');
-  var scrollTop = 0;
-  var nowIdx = 0;
-  var arrTopVal = [];
+  const header = document.querySelector('header');
+  const mnu = document.querySelectorAll('header > .container > nav > .gnb > li > a');
+  let scrollTop = 0;
+  let nowIdx = 0;
+  const arrTopVal = [];
 
-  $('section').each(function(idx){
-    arrTopVal[idx] = $(this).offset().top;
+  // 각 section 요소의 offsetTop 값을 배열에 저장
+  document.querySelectorAll('section').forEach(function(section, idx) {
+    arrTopVal[idx] = section.offsetTop;
   });
   console.log(arrTopVal);
 
-  //header
-  $mnu.on('click',function(event){
-    event.preventDefault();
-    nowIdx = $mnu.index(this);
+  // 메뉴 클릭 시
+  mnu.forEach(function(menuItem, idx) {
+    menuItem.addEventListener('click', function(event) {
+      event.preventDefault();
+      nowIdx = idx;
 
-    $mnu.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
-    $('html,body').stop().animate({
-      scrollTop : arrTopVal[nowIdx]
-    },500,'easeInOutCubic');
+      // 클릭한 메뉴의 부모 li에 'on' 클래스 추가 및 형제 li에서 제거
+      mnu.forEach(function(item, i) {
+        if (i === nowIdx) {
+          item.parentElement.classList.add('on');
+        } else {
+          item.parentElement.classList.remove('on');
+        }
+      });
+
+      // 스크롤 애니메이션 (부드럽게 스크롤)
+      window.scrollTo({
+        top: arrTopVal[nowIdx],
+        behavior: 'smooth'
+      });
+    });
   });
 
-  $(window).on('scroll',function(){
-    scrollTop = $(this).scrollTop();
+  window.addEventListener('scroll', function() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if(scrollTop>arrTopVal[0]){
-      $header.addClass('active');
-    }else{
-      $header.removeClass('active');
+    if (scrollTop > arrTopVal[0]) {
+      header.classList.add('active');
+    } else {
+      header.classList.remove('active');
     }
 
-    for(var i=0; i<6; i++){
-      if(scrollTop>=arrTopVal[i]){ 
-        $mnu.eq(i).parent().addClass('on').siblings().removeClass('on');
+    for (let i = 0; i < arrTopVal.length; i++) {
+      if (scrollTop >= arrTopVal[i]) {
+        mnu.forEach((item, j) => {
+          if (i === j) {
+            item.parentElement.classList.add('on');
+          } else {
+            item.parentElement.classList.remove('on');
+          }
+        });
       }
     }
-  });//end of header
+  });
 
-  if ($(window).width() < 768) {
-    // 모바일 화면에서 추가적인 작업 수행
-    // 예를 들어, 메뉴를 터치할 때 메뉴를 숨기는 기능 추가 등
+  if (window.innerWidth < 768) {
+    // 모바일 화면에서 추가 작업 가능
   }
-
-});//end of header handler
-
-
-
-
+});
